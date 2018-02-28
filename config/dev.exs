4515a8da1fb9e3e22828a0dc6b5d65fba3b20af8
Mickey <poverwhelming@gmail.com>
2018-02-28 14:30:01 +0800

    setup db

--- config/dev.exs
+++ config/dev.exs
@@ -6,7 +6,7 @@ use Mix.Config
 # The watchers configuration can be used to run external
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
-config :live_auction, LiveAuctionWeb.Endpoint,
+config :cassius, CassiusWeb.Endpoint,
   http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
@@ -31,13 +31,13 @@ config :live_auction, LiveAuctionWeb.Endpoint,
 # different ports.
 
 # Watch static and templates for browser reloading.
-config :live_auction, LiveAuctionWeb.Endpoint,
+config :cassius, CassiusWeb.Endpoint,
   live_reload: [
     patterns: [
       ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
       ~r{priv/gettext/.*(po)$},
-      ~r{lib/live_auction_web/views/.*(ex)$},
-      ~r{lib/live_auction_web/templates/.*(eex)$}
+      ~r{lib/cassius_web/views/.*(ex)$},
+      ~r{lib/cassius_web/templates/.*(eex)$}
     ]
   ]
 
@@ -49,10 +49,8 @@ config :logger, :console, format: "[$level] $message\n"
 config :phoenix, :stacktrace_depth, 20
 
 # Configure your database
-config :live_auction, LiveAuction.Repo,
+config :cassius, Cassius.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "live_auction_dev",
   hostname: "localhost",
   pool_size: 10
