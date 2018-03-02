ba6e89aff365462bffe50425b7484d087123b50f
Mickey <poverwhelming@gmail.com>
2018-03-02 16:19:55 +0800

    got stylesheet compiling with webpack

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,12 +1,89 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack');
 
-const config = {
-  mode: 'development',
-  entry: './js/app.js',
-  output: {
-    path: path.resolve(__dirname, 'static/js'),
-    filename: 'bundle.js'
+const config_fn = (env) => {
+  const isDev = !(env && env.prod);
+  const devtool = isDev ? "eveal" : "source-map";
+
+  return {
+    mode: "development",
+
+    devtool: devtool,
+
+    context: __dirname,
+
+    entry: {
+      app: [
+        "./js/app.js",
+        "./scss/app.scss"
+      ]
+    },
+
+    output: {
+      path: path.resolve(__dirname, "../priv/static"),
+      filename: 'js/[name].js',
+      publicPath: 'http://localhost:8080/'
+    },
+
+    devServer: {
+      headers: {
+        "Access-control-Allow-Origin": "*"
+      }
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(css|scss)$/,
+          exclude: /node_modules/,
+          use: isDev ? [
+            "style-loader",
+            "css-loader",
+            "sass-loader"
+          ] : ExtractTextPlugin.extract({
+            fallback: "sass-loader",
+            use: ['css-loader', 'sass-loader']
+          })
+        }
+      ]
+    },
+    plugins: isDev ? [
+      new CopyWebpackPlugin([{
+        from: "./static",
+        to: path.resolve(__dirname, "../priv/static")
+      }])
+    ] : [
+      new CopyWebpackPlugin([{
+        from: "./static",
+        to: path.resolve(__dirname, "../priv/static")
+      }]),
+
+      new ExtractTextPlugin({
+        filename: "css/[name].css",
+        allChunks: true
+      }),
+
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true,
+        beautify: false,
+        comments: false,
+        extractComments: false,
+        compress: {
+          warnings: false,
+          drop_console: true
+        },
+        mangle: {
+          except: ['$'],
+          screw_ie8: true,
+          keep_fname: true
+        }
+
+      })
+    ]
   }
 };
 
-module.exports = config;
+module.exports = config_fn;
--- config/dev.exs
+++ config/dev.exs
@@ -11,8 +11,11 @@ config :live_auction, LiveAuctionWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/webpack/bin/webpack.js", "--watch",
-                    cd: Path.expand("../assets", __DIR__)]]
+  watchers: [node: [
+    "node_modules/.bin/webpack-dev-server", "--inline", "--colors", "--hot", "--stdin",
+    "--host", "localhost", "--port", "8080", "--public", "localhost:8080",
+    cd: Path.expand("../assets", __DIR__)
+  ]]
 
 # ## SSL Support
 #
