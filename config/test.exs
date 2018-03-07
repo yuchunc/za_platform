7946c2f3a3713b6d3c7bdacf9597875a191b34ca
Mickey <poverwhelming@gmail.com>
2018-03-07 17:54:07 +0800

    added test.watch with some house cleaning

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,6 @@ config :logger, level: :warn
 # Configure your database
 config :live_auction, LiveAuction.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "live_auction_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
