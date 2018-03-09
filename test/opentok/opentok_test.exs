786869b47150961f5a829fdc87fbe54ae3a2d02f
Mickey <poverwhelming@gmail.com>
2018-03-09 17:09:03 +0800

    align myself with tests

--- test/opentok/opentok_test.exs
+++ test/opentok/opentok_test.exs
@@ -8,8 +8,16 @@ defmodule OpenTok.OpenTokTest do
   @tag :skip
   describe "create_session/0" do
     test "creates a session from config" do
-      assert OpenTok.create_session
-      |> IO.inspect(label: "session")
+      assert {:ok, _session_id} = OpenTok.create_session
+    end
+  end
+
+  @tag :skip
+  describe "generate_toke/1" do
+    test "generates some token from session_id" do
+      {:ok, _session_id} = OpenTok.create_session
+
+      assert {:ok, _some_token} = OpenTok.generate_token
     end
   end
 end
