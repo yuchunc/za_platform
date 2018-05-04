307b2e6cae7abaab7d5ef3dca3665f7a1d895688
Mickey <poverwhelming@gmail.com>
2018-05-04 14:38:27 +0800

    ini stream when landed on streaming page

--- lib/live_auction_web/controllers/streaming_controller.ex
+++ lib/live_auction_web/controllers/streaming_controller.ex
@@ -6,9 +6,8 @@ defmodule LiveAuctionWeb.StreamingController do
   def show(conn, _params) do
     with %User{} = user <- current_resource(conn),
          {:ok, stream} <- Streaming.new_session(user.id),
-         {:ok, _key, token} <- OpenTok.generate_token(stream.ot_session_id, :publisher, "publisher1"),
-         ot_config <- OpenTok.Util.get_config,
-         opentok_params <- %{session_id: stream.ot_session_id, token: token, config: ot_config}
+         {:ok, key, token} <- OpenTok.generate_token(stream.ot_session_id, :publisher, "publisher1"),
+         opentok_params <- %{session_id: stream.ot_session_id, token: token, key: key}
     do
       render(conn, "show.html", opentok_params: opentok_params)
     end
