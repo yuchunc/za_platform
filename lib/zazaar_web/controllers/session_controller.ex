16e51de80a19a355036e7248e8079ab11c9f15a9
Mickey <poverwhelming@gmail.com>
2018-09-19 15:31:53 +0800

    temp debug commit

--- lib/zazaar_web/controllers/session_controller.ex
+++ lib/zazaar_web/controllers/session_controller.ex
@@ -1,6 +1,8 @@
 defmodule ZaZaarWeb.SessionController do
   use ZaZaarWeb, :controller
 
+  require Logger
+
   plug(Ueberauth)
 
   action_fallback(FallbackController)
@@ -37,6 +39,8 @@ defmodule ZaZaarWeb.SessionController do
     {:ok, user} = Account.fb_auth(auth.uid, auth.info)
 
     Streaming.find_or_create_channel(user)
+
+    Logger.debug "conn: #{conn}"
 
     conn
     |> Guardian.Plug.sign_in(user)
