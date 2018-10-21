2cb6ecb27797b317448c69eb8101581b57a6f2a0
Mickey <poverwhelming@gmail.com>
2018-10-21 18:04:26 +0800

    get comment to send on enter, and clear msg box

--- assets/js/views/util/comment.js
+++ assets/js/views/util/comment.js
@@ -1,17 +1,28 @@
 export default (channel) => {
-  document.getElementById('send-comment').onclick = (_) =>{
-    const message = document.getElementById('user-comment').value;
+  let msgBox = document.getElementById('user-comment');
 
-    if(message) {
-      channel.push("stream:send_comment", {comment: message});
-    }
+  document.getElementById('send-comment').onclick = (_) =>{
+    sendComment();
   };
+
+  msgBox.addEventListener('keydown', (e) => {
+    if(e.which == 13) {
+      sendComment();
+    };
+  });
 
   channel.on("stream:comment_sent", (resp) => {
     let log = document.getElementById('chat-log');
     let commentPanel = document.createElement("div");
     commentPanel.className = "panel-block";
     commentPanel.innerHTML = resp.user_name + "：" + resp.comment.content;
+    msgBox.value = "";
     log.prepend(commentPanel);
   });
+
+  const sendComment = () => {
+    if(msgBox.value) {
+      channel.push("stream:send_comment", {comment: msgBox.value});
+    };
+  };
 };
