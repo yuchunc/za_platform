c6d73ad8f2c9b48c21c0655878d7cce787daaf0c
Mickey <poverwhelming@gmail.com>
2018-10-29 17:29:17 +0800

    dropped channels table

--- priv/repo/migrations/20181028065801_add_streamer_id_to_stream.exs
+++ priv/repo/migrations/20181028065801_add_streamer_id_to_stream.exs
@@ -2,9 +2,11 @@ defmodule ZaZaar.Repo.Migrations.AddStreamerIdToStream do
   use Ecto.Migration
 
   def change do
+    drop_if_exists index("streams", [:channel_id])
+
     alter table(:streams) do
       add :streamer_id, references(:users, type: :uuid, on_delete: :nothing)
-      modify :channel_id, :uuid, null: true
+      remove :channel_id
     end
 
     rename table(:streams), :facebook_stream_key, to: :fb_stream_key
---
+++ priv/repo/migrations/20181029092354_drop_channels.exs
@@ -0,0 +1,7 @@
+defmodule ZaZaar.Repo.Migrations.DropChannels do
+  use Ecto.Migration
+
+  def change do
+    drop table("channels")
+  end
+end
