6a272a0fd5379eac6027e359520f73ccd70934fd
Mickey <poverwhelming@gmail.com>
2018-10-31 16:46:48 +0800

    add js for follow button

--- assets/js/views/live_stream/show.js
+++ assets/js/views/live_stream/show.js
@@ -4,11 +4,13 @@ import commentAction from '../util/comment';
 
 const stream_id = window.streamConfig.stream_id;
 
+let followBtn = document.getElementById('viewer-follow-btn');
+
 const startSubscribing = (ot_config) => {
   const session = OT.initSession(ot_config.key, ot_config.session_id)
 
-  session.on("streamCreated", function(event) {
-    console.log("event", event);
+  session.on('streamCreated', function(event) {
+    console.log('event', event);
     session.subscribe(event.stream, 'stream-view', {
       insertMode: 'append',
       width: '100%',
@@ -25,17 +27,34 @@ export default () => {
     mount: () => {
       console.log('LiveStream Show mounted');
 
-      let channel = socket.channel("stream:" + stream_id);
+      let channel = socket.channel('stream:' + stream_id);
       channel.join();
 
       channel
-        .push("viewer:join", {})
-        .receive("ok", (resp) => {
-          // TODO uncomment this
+        .push('viewer:join', {})
+        .receive('ok', (resp) => {
           startSubscribing(resp);
         });
 
       commentAction(channel);
+
+      followBtn.addEventListener('click', () => {
+        if(followBtn.classList.contains('following')) {
+          channel
+            .push('viewer:stop_follow')
+            .receive('ok', () => {
+              followBtn.classList.remove('following');
+              followBtn.innerValue = '跟隨';
+            });
+        } else {
+          channel
+            .push('viewer:start_following')
+            .receive('ok', () => {
+              followBtn.classList.add('following');
+              followBtn.innerValue = '已跟隨';
+            });
+        };
+      });
     },
 
     unmount: () => {
