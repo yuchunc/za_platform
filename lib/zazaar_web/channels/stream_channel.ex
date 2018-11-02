cd2b73932ca84e1e8585642f1c422df6eb007402
Mickey <poverwhelming@gmail.com>
2018-11-02 17:14:52 +0800

    displace viewer count in LiveStream#index

--- lib/zazaar_web/channels/stream_channel.ex
+++ lib/zazaar_web/channels/stream_channel.ex
@@ -23,7 +23,7 @@ defmodule ZaZaarWeb.StreamChannel do
   def handle_info({:after_join, payload}, socket) when payload == %{} do
     broadcast(socket, "user:joined", payload)
 
-    Presence.track(socket, "anom:" <> Ecto.UUID.generate(), %{
+    Presence.track(socket, "anon:" <> Ecto.UUID.generate(), %{
       online_at: inspect(System.system_time(:seconds))
     })
 
--- lib/zazaar_web/controllers/live_stream_controller.ex
+++ lib/zazaar_web/controllers/live_stream_controller.ex
@@ -3,11 +3,13 @@ defmodule ZaZaarWeb.LiveStreamController do
 
   action_fallback(FallbackController)
 
+  alias ZaZaarWeb.StreamPresence, as: Presence
+
   def index(conn, _params) do
     with streams0 <- Streaming.get_streams(order_by: [desc: :archived_at, desc: :inserted_at]),
          streamer_ids <- Enum.map(streams0, & &1.streamer_id),
          users <- Account.get_users(streamer_ids),
-         streams1 <- append_streamer(streams0, users) do
+         streams1 <- append_info(streams0, users) do
       render(conn, "index.html", streams: streams1)
     end
   end
@@ -15,16 +17,19 @@ defmodule ZaZaarWeb.LiveStreamController do
   def show(conn, %{"id" => stream_id}) do
     with stream0 <- Streaming.get_stream(stream_id),
          comments <- include_user_names(stream0.comments) do
-      stream1 = Map.replace(stream0, :comments, comments)
+      stream1 = Map.put(stream0, :comments, comments)
       user = current_resource(conn) || %User{}
       render(conn, "show.html", stream: stream1, user: user)
     end
   end
 
-  defp append_streamer(streams, users) do
+  defp append_info(streams, users) do
     Enum.map(streams, fn stream ->
       user = Enum.find(users, &(&1.id == stream.streamer_id))
-      Map.put_new(stream, :streamer, user)
+
+      stream
+      |> Map.put_new(:streamer, user)
+      |> Map.put_new(:viewer_count, Presence.list("stream:" <> stream.id) |> Enum.count())
     end)
   end
 
