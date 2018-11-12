bcaf349cf39a32ecace283a7ac535b48c05fd84d
Mickey <poverwhelming@gmail.com>
2018-11-12 16:20:18 +0800

    fixed presence with null element in js

--- assets/js/views/util/streamPresences.js
+++ assets/js/views/util/streamPresences.js
@@ -1,15 +1,15 @@
 import {Presence} from 'phoenix';
 
 export default (channel, presences, element) => {
+  if(element !== null) {
+    channel.on("presence_state", state => {
+      presences = Presence.syncState(presences, state);
+      element.innerHTML = Object.keys(presences).length;
+    })
 
-  channel.on("presence_state", state => {
-    presences = Presence.syncState(presences, state);
-    element.innerHTML = Object.keys(presences).length;
-  })
-
-  channel.on("presence_diff", diff => {
-    presences = Presence.syncDiff(presences, diff);
-    element.innerHTML = Object.keys(presences).length;
-  });
-
+    channel.on("presence_diff", diff => {
+      presences = Presence.syncDiff(presences, diff);
+      element.innerHTML = Object.keys(presences).length;
+    });
+  };
 };
